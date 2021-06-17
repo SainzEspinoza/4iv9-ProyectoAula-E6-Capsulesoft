cc0727a50351ad4d62982f0369474a8df44d77ef
cxlebd <dsanchezg1903@alumno.ipn.mx>
2021-06-17 02:30:09 -0500

    Correciones

--- Capsulesoft_equipo6/web/js/validacionFormulario.js
+++
@@ -1,8 +0,0 @@
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
-
-const expresiones = {
-    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-}
-
-formmulario.addEventListener('submit')
