69f371617b62ac0ca44e71f0fbdc8235ba204456
cxlebd <dsanchezg1903@alumno.ipn.mx>
2021-06-18 06:03:29 -0500

    Validación completa en registro paciente ;D

--- Capsulesoft_equipo6/build/web/js/validarPaciente.js
+++ Capsulesoft_equipo6/build/web/js/validarPaciente.js
@@ -1,8 +1,15 @@
 function validar() {
     var nombre, apmat, appat, peso, estatura, pad, tipoSangre, usuario, contrasena, contrasena2;
-    var expresion;
+    
+    const exp = {
+        nombreE: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+        passE: /^.{4,12}$/,
+	    correoE: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    }
 
-    nombre = document.getElementById('no_usu').value;
+    expNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+
+    nombre = document.getElementById('nom_usu').value;
     apmat = document.getElementById('mat_usu').value;
     appat = document.getElementById('pat_usu').value;
     peso = document.getElementById('peso_usu').value;
@@ -11,11 +18,23 @@ function validar() {
     tipoSangre = document.getElementById('sangre_usu').value;
     usuario = document.getElementById('usu').value;
     contrasena = document.getElementById('pass_usu').value;
-    contrasena2 = document.getElementById('pass2_usu').value;
+    contrasena2 = document.getElementById('pass2_usu').value;    
+
+    /*alert(nombre);
+    alert(contrasena);
+    alert(contrasena2);*/
 
     if(nombre === "" || apmat === "" || appat === "" || 
     peso === "" || estatura === "" || pad === "" || 
     tipoSangre === "" || usuario === "" || contrasena === "" || contrasena2 === ""){
-        alert('me comi una salchipapa')
+        alert('Todos los campos son obligatorios');
+        return false;
+    }
+    else if(contrasena =! contrasena2){
+        alert('Las contraseñas no coinciden.');
+    }
+    else if(expNombre.test(nombre)){
+        alert('Nombre no válido.');
+        return false;
     }
 }
